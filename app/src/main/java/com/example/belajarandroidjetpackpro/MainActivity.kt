660af994bcb6c8eaf65616e8a8b3dd883a0843ac
gadogado092 <gadogado092>
2021-04-03 14:16:12 +0800

    add function calculate UI

--- app/src/main/java/com/example/belajarandroidjetpackpro/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroidjetpackpro/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.belajarandroidjetpackpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.belajarandroidjetpackpro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        displayResult()
+
+        activityMainBinding.btnCalculate.setOnClickListener {
+            val width = activityMainBinding.edtWidth.text.toString()
+            val height = activityMainBinding.edtHeight.text.toString()
+            val length = activityMainBinding.edtLength.text.toString()
+            when {
+                width.isEmpty() -> {
+                    activityMainBinding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    activityMainBinding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    activityMainBinding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    mainViewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        activityMainBinding.tvResult.text = mainViewModel.result.toString()
     }
 }
