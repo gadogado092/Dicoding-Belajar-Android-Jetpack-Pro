9c0204b09cacd5f646cd73c92c03a4aae6702b54
gadogado092 <gadogado092>
2021-04-07 17:59:24 +0800

    add data class and change to parceable

--- app/src/main/java/com/example/belajarandroidjetpackpro/data/source/remote/response/ModuleResponse.kt
+++ app/src/main/java/com/example/belajarandroidjetpackpro/data/source/remote/response/ModuleResponse.kt
@@ -4,10 +4,9 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class CourseResponse(
-    var id: String,
+data class ModuleResponse(
+    var moduleId: String,
+    var courseId: String,
     var title: String,
-    var description: String,
-    var date: String,
-    var imagePath: String
-): Parcelable
+    var position: Int
+) : Parcelable
