176daa5f0b46f28f2be12e65463c9702c3771665
gadogado092 <gadogado092>
2021-04-08 12:09:42 +0800

    finish

--- app/src/main/java/com/example/belajarandroidjetpackpro/MainActivity.kt
+++ app/src/main/java/com/example/belajarandroidjetpackpro/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.belajarandroidjetpackpro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.belajarandroidjetpackpro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mLiveDataTimerModel: MainViewModel
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        mLiveDataTimerModel = ViewModelProvider(this)[MainViewModel::class.java]
+        subscribe()
+    }
+
+    private fun subscribe(){
+        val elapsedTimeObserver = Observer<Long?>{ aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            activityMainBinding.timerTextview.text = newText
+        }
+
+        mLiveDataTimerModel.getElapsedTime().observe(this, elapsedTimeObserver)
     }
 }
