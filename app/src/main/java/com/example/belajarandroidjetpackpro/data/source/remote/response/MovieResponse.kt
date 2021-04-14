9fe22c51650003e47442389e6e019893e40c1011
gadogado092 <gadogado092>
2021-04-14 16:44:51 +0800

    perbaikan setelah review

--- app/src/main/java/com/example/belajarandroidjetpackpro/data/source/remote/response/MovieResponse.kt
+++ app/src/main/java/com/example/belajarandroidjetpackpro/data/source/remote/response/MovieResponse.kt
@@ -1,13 +1,9 @@
 package com.example.belajarandroidjetpackpro.data.source.remote.response
 
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@Parcelize
 data class MovieResponse(
     var id: String,
     var title: String,
     var dateRelease: String,
     var pathImage: String,
     var description: String,
-) : Parcelable
+)
